3ec8061598b8955997cba962fa34135c25f65a89
zisov4eto <zisov4eto@gmail.com>
2017-03-28 15:28:59 +0300

    Configurate AutoMapper

---
+++ BaskervilleWebsite/Baskerville.App/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Baskerville.Models.DataModels;
+using Baskerville.Models.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Baskerville.App.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {           
+            this.CreateMap<Subscriber, SubscriberDto>();
+        }
+    }
+}
