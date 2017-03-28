3b5bcae8acccd6007db3c3da70f556ce1975719a
zisov4eto <zisov4eto@gmail.com>
2017-03-28 15:52:33 +0300

    Create mapping profile for service project

---
+++ BaskervilleWebsite/Baskerville.Services/ServiceMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Baskerville.Models.DataModels;
+using Baskerville.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baskerville.Services
+{
+    public class ServiceMappingProfile : Profile
+    {
+        public ServiceMappingProfile()
+        {
+            this.CreateMap<Subscriber, SubscriberViewModel>();
+        }
+    }
+}
