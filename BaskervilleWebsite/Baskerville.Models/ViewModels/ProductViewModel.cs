72539f11bc9660f768156e2ae43ac1fd027c36a5
zisov4eto <zisov4eto@gmail.com>
2017-04-01 18:49:49 +0300

    Implement service side validation for products.

--- BaskervilleWebsite/Baskerville.Models/ViewModels/ProductViewModel.cs
+++ BaskervilleWebsite/Baskerville.Models/ViewModels/ProductViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Baskerville.Models.DataModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,22 +19,38 @@ namespace Baskerville.Models.ViewModels
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "Дължината трябва да е между 2 и 255 символа")]
+        [Display(Name = "Име (БГ)")]
         public string NameBg { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "Дължината трябва да е между 2 и 255 символа")]
+        [Display(Name = "Име (АНГ)")]
         public string NameEn { get; set; }
 
+        [Display(Name = "Описание (БГ)")]
         public string DescriptionBg { get; set; }
 
+        [Display(Name = "Описание (АНГ)")]
         public string DescriptionEn { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Количество")]
         public double Quantity { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Цена")]
         public double Price { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Категория")]
         public int PrimaryCategoryId { get; set; }
 
+        [Display(Name = "Подкатегория")]
         public int? SubcategoryId { get; set; }
 
+        [Display(Name = "Публично")]
         public bool IsPublic { get; set; }
 
         public IEnumerable<ProductCategory> PrimaryCategories { get; set; }
