b9393ee392a6fd197c7db1834cdf01c667d9f70e
zisov4eto <zisov4eto@gmail.com>
2017-04-03 00:11:30 +0300

    Add Admin area.

---
+++ BaskervilleWebsite/Baskerville.App/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,77 @@
+﻿using Baskerville.Data;
+using Baskerville.Models.ViewModels;
+using Baskerville.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baskerville.App.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ProductsService service;
+
+        public ProductsController()
+        {
+            this.service = new ProductsService(new BaskervilleContext());
+        }
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var model = this.service.GetAllProducts();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var model = this.service.GetProduct(id);
+            if (model == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            return View("ProductForm", model);
+        }
+
+        public ActionResult Create()
+        {
+            var model = this.service.GetEmptyProduct();
+            return this.View("ProductForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            this.service.RemoveProduct(id);
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(ProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.PrimaryCategories = this.service.GetPrimaryCategories();
+                return View("ProductForm", model);
+            }
+
+            //Model is newly created and shoot be added to the database.
+            if (model.Id == 0)
+                this.service.CreateProduct(model);
+            else
+                this.service.UpdateProduct(model);
+                
+            return this.RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult GetSubCategories(int id)
+        {
+            var subCategories = this.service.GetSubCategories(id);
+            return Json(subCategories, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ BaskervilleWebsite/Baskerville.App/Areas/Admin/Controllers/SubscribersController.cs
@@ -0,0 +1,36 @@
+﻿using Baskerville.Data;
+using Baskerville.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baskerville.App.Areas.Admin.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        private SubscribersService service;
+
+        public SubscribersController()
+        {
+            this.service = new SubscribersService(new BaskervilleContext());
+        }
+
+        // GET: Subscribers
+        public ActionResult Index()
+        {
+            var model = this.service.GetActiveSubscribers();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            this.service.RemoveSubscriber(id);
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+    }
+}
