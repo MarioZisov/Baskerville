884f5b67e7b749495964ca72582b78e03105d920
zisov4eto <zisov4eto@gmail.com>
2017-04-06 20:24:12 +0300

    Add service and client side validation for evetns form.

--- BaskervilleWebsite/Baskerville.Models/ViewModels/EventViewModel.cs
+++ BaskervilleWebsite/Baskerville.Models/ViewModels/EventViewModel.cs
@@ -1,27 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Baskerville.Models.ViewModels
 {
     public class EventViewModel
     {
+        private DateTime? startDate;
+
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Име (БГ)")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Името трябва да е между 5 и 50 символа")]
         public string NameBg { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Име (АНГ)")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Името трябва да е между 5 и 50 символа")]
         public string NameEn { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Описание (БГ)")]
+        [StringLength(500, MinimumLength = 5, ErrorMessage = "Описанието трябва да е между 5 и 500 символа")]
         public string DescriptionBg { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Описание (АНГ)")]
+        [StringLength(500, MinimumLength = 5, ErrorMessage = "Описанието трябва да е между 5 и 500 символа")]
         public string DescriptionEn { get; set; }
 
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Снимка (URL)")]
         public string ImageUrl { get; set; }
 
-        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Полето е задължително")]
+        [Display(Name = "Начало")]
+        public DateTime? StartDate
+        {
+            get { return this.startDate ?? DateTime.Now; }
+            set { this.startDate = value; }
+        }
 
+        [Display(Name = "Публично")]
         public bool IsPublic { get; set; }
     }
 }
