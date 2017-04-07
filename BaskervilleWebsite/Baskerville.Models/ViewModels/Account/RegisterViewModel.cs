0b47833fdf9e51d83b9cebf86667e45163ed77c5
zisov4eto <zisov4eto@gmail.com>
2017-04-07 13:24:30 +0300

    Translate register view

--- BaskervilleWebsite/Baskerville.Models/ViewModels/Account/RegisterViewModel.cs
+++ BaskervilleWebsite/Baskerville.Models/ViewModels/Account/RegisterViewModel.cs
@@ -11,23 +11,23 @@ namespace Baskerville.Models.ViewModels.Account
             this.RolesNames = new List<string>();
         }
 
-        [Required]
+        [Required(ErrorMessage = Constants.RequiredFildMessage)]
         [MinLength(2), MaxLength(255)]
         [Display(Name = "Потребител")]
         public string Username { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = Constants.RequiredFildMessage)]
+        [StringLength(100, ErrorMessage = "Паралота трябва да бъде поне {2} символа", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Парола")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Потвърди парола")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "Паролите не съвпадат")]
         public string ConfirmPassword { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = Constants.RequiredFildMessage)]
         [Display(Name = "Роля")]
         public string RoleName { get; set; }
 
