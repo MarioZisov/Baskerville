79a2af07d676ebc52533828b4800dfd08aba0ad8
zisov4eto <zisov4eto@gmail.com>
2017-04-07 13:16:51 +0300

    Translate log in view.

--- BaskervilleWebsite/Baskerville.App/Areas/Admin/Controllers/AccountController.cs
+++ BaskervilleWebsite/Baskerville.App/Areas/Admin/Controllers/AccountController.cs
@@ -88,7 +88,7 @@ namespace Baskerville.App.Areas.Admin.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Невалиден опит за влизане в системата");
                     return View(model);
             }
         }
--- BaskervilleWebsite/Baskerville.Models/ViewModels/Account/LoginViewModel.cs
+++ BaskervilleWebsite/Baskerville.Models/ViewModels/Account/LoginViewModel.cs
@@ -4,11 +4,11 @@ namespace Baskerville.Models.ViewModels.Account
 {
     public class LoginViewModel
     {
-        [Required]
+        [Required(ErrorMessage = Constants.RequiredFildMessage)]
         [Display(Name = "Потребител")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = Constants.RequiredFildMessage)]
         [DataType(DataType.Password)]
         [Display(Name = "Парола")]
         public string Password { get; set; }
