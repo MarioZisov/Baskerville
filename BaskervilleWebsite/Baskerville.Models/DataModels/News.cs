ee18c3d733d6e4314512dae1615f279b44005865
zisov4eto <zisov4eto@gmail.com>
2017-04-24 00:56:53 +0300

    Add News model to Db.

---
+++ BaskervilleWebsite/Baskerville.Models/DataModels/News.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baskerville.Models.DataModels
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string TitleBg { get; set; }
+
+        public string TitleEn { get; set; }
+
+        public string MessageBg { get; set; }
+
+        public string MessageEn { get; set; }
+
+        public string FromBg { get; set; }
+
+        public string FromEn { get; set; }
+
+        public bool IsRemoved { get; set; }
+
+        public bool IsPublic { get; set; }
+    }
+}
