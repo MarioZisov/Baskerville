1055e8092952a88b2bcddbe7dc0ce8a2889c8d23
zisov4eto <zisov4eto@gmail.com>
2017-04-25 13:52:26 +0300

    Create view model for tracking last logs

---
+++ BaskervilleWebsite/Baskerville.Models/ViewModels/LastLogsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baskerville.Models.ViewModels
+{
+    public class LastLogsViewModel
+    {
+        public string Username { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
