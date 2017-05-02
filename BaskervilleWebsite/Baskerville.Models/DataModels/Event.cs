bbe9b65142169761ce90400539cbdbfccd280634
zisov4eto <zisov4eto@gmail.com>
2017-05-02 12:57:25 +0300

    Add Db validation for Event model

--- BaskervilleWebsite/Baskerville.Models/DataModels/Event.cs
+++ BaskervilleWebsite/Baskerville.Models/DataModels/Event.cs
@@ -1,25 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Baskerville.Models.DataModels
+﻿namespace Baskerville.Models.DataModels
 {
+    using Constants;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
     public class Event
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxEventNameLegnth, 
+            MinimumLength = AdminMessages.MinEventNameLegnth)]
         public string NameBg { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxEventNameLegnth,
+            MinimumLength = AdminMessages.MinEventNameLegnth)]
         public string NameEn { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxEventDescriptionLegnth, 
+            MinimumLength = AdminMessages.MinEventDescriptionLegnth)]
         public string DescriptionBg { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxEventDescriptionLegnth,
+            MinimumLength = AdminMessages.MinEventDescriptionLegnth)]
         public string DescriptionEn { get; set; }
 
         public DateTime StartDate { get; set; }
 
+        [Required]
         public string ImageUrl { get; set; }
 
         public bool IsRemoved { get; set; }
