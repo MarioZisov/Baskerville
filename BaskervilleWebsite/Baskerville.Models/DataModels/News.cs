86d454d06b19d97a188350b1c0fcb15c9255af5f
zisov4eto <zisov4eto@gmail.com>
2017-05-02 13:18:04 +0300

    Add Db validation for News model

--- BaskervilleWebsite/Baskerville.Models/DataModels/News.cs
+++ BaskervilleWebsite/Baskerville.Models/DataModels/News.cs
@@ -1,25 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Baskerville.Models.DataModels
+﻿namespace Baskerville.Models.DataModels
 {
+    using Constants;
+    using System.ComponentModel.DataAnnotations;
+
     public class News
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxTitleLength,
+            MinimumLength = AdminMessages.MinTitleLength)]
         public string TitleBg { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxTitleLength,
+            MinimumLength = AdminMessages.MinTitleLength)]
         public string TitleEn { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxNewsLength,
+            MinimumLength = AdminMessages.MinNewsLength)]
         public string MessageBg { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxNewsLength,
+            MinimumLength = AdminMessages.MinNewsLength)]
         public string MessageEn { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxFromLength,
+            MinimumLength = AdminMessages.MinFromLength)]
         public string FromBg { get; set; }
 
+        [Required]
+        [StringLength(
+            maximumLength: AdminMessages.MaxFromLength,
+            MinimumLength = AdminMessages.MinFromLength)]
         public string FromEn { get; set; }
 
         public bool IsRemoved { get; set; }
--- BaskervilleWebsite/Baskerville.Models/ViewModels/NewsViewModel.cs
+++ BaskervilleWebsite/Baskerville.Models/ViewModels/NewsViewModel.cs
@@ -1,13 +1,8 @@
-﻿using Baskerville.Models.Constants;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Baskerville.Models.ViewModels
+﻿namespace Baskerville.Models.ViewModels
 {
+    using Constants;
+    using System.ComponentModel.DataAnnotations;
+
     public class NewsViewModel
     {
         public int Id { get; set; }
@@ -16,32 +11,50 @@ namespace Baskerville.Models.ViewModels
         public bool IsPublic { get; set; }
 
         [Required(ErrorMessage = AdminMessages.RequiredFieldMessage)]
-        [StringLength(maximumLength: AdminMessages.MaxTitleLength, MinimumLength = AdminMessages.MinTitleLength, ErrorMessage = AdminMessages.TitleLengthMessage)]
+        [StringLength(
+            maximumLength: AdminMessages.MaxTitleLength, 
+            MinimumLength = AdminMessages.MinTitleLength, 
+            ErrorMessage = AdminMessages.TitleLengthMessage)]
         [Display(Name = "Заглавие (бг)")]
         public string TitleBg { get; set; }
 
         [Required(ErrorMessage = AdminMessages.RequiredFieldMessage)]
-        [StringLength(maximumLength: AdminMessages.MaxNewsLength, MinimumLength = AdminMessages.MinNewsLength, ErrorMessage = AdminMessages.NewsLengthMessage)]
+        [StringLength(
+            maximumLength: AdminMessages.MaxNewsLength, 
+            MinimumLength = AdminMessages.MinNewsLength, 
+            ErrorMessage = AdminMessages.NewsLengthMessage)]
         [Display(Name = "Съобщение (бг)")]
         public string MessageBg { get; set; }
 
         [Required(ErrorMessage = AdminMessages.RequiredFieldMessage)]
-        [StringLength(maximumLength: AdminMessages.MaxFromLength, MinimumLength = AdminMessages.MinFromLength, ErrorMessage = AdminMessages.FromLengthMessage)]
+        [StringLength(
+            maximumLength: AdminMessages.MaxFromLength, 
+            MinimumLength = AdminMessages.MinFromLength,
+            ErrorMessage = AdminMessages.FromLengthMessage)]
         [Display(Name = "От (бг)")]
         public string FromBg { get; set; }
 
         [Required(ErrorMessage = AdminMessages.RequiredFieldMessage)]
-        [StringLength(maximumLength: AdminMessages.MaxTitleLength, MinimumLength = AdminMessages.MinTitleLength, ErrorMessage = AdminMessages.TitleLengthMessage)]
+        [StringLength(
+            maximumLength: AdminMessages.MaxTitleLength,
+            MinimumLength = AdminMessages.MinTitleLength, 
+            ErrorMessage = AdminMessages.TitleLengthMessage)]
         [Display(Name = "Заглавие (анг)")]
         public string TitleEn { get; set; }
 
         [Required(ErrorMessage = AdminMessages.RequiredFieldMessage)]
-        [StringLength(maximumLength: AdminMessages.MaxNewsLength, MinimumLength = AdminMessages.MinNewsLength, ErrorMessage = AdminMessages.NewsLengthMessage)]
+        [StringLength(
+            maximumLength: AdminMessages.MaxNewsLength, 
+            MinimumLength = AdminMessages.MinNewsLength, 
+            ErrorMessage = AdminMessages.NewsLengthMessage)]
         [Display(Name = "Съобщение (анг)")]
         public string MessageEn { get; set; }
 
         [Required(ErrorMessage = AdminMessages.RequiredFieldMessage)]
-        [StringLength(maximumLength: AdminMessages.MaxFromLength, MinimumLength = AdminMessages.MinFromLength, ErrorMessage = AdminMessages.FromLengthMessage)]
+        [StringLength(
+            maximumLength: AdminMessages.MaxFromLength, 
+            MinimumLength = AdminMessages.MinFromLength, 
+            ErrorMessage = AdminMessages.FromLengthMessage)]
         [Display(Name = "От (анг)")]
         public string FromEn { get; set; }
     }
